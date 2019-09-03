fc767dca40884be1ce41624f97fa15e106069986
Fredrik Lindeberg <flindeberg@gmail.com>
2019-09-03 15:34:24 +0200

    Started on parsing script

---
+++ scripts/parsebview.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+## Uses a bview file (such as http://data.ris.ripe.net/rrc00/latest-bview.gz), i.e. not updates but the entire view, and generates graphs from that.
+
+
+## First argument is bview-file. The rest is automatic
+
