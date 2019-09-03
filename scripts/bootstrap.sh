ae2adfdba0ebf91e1d83ce42ebc3838b9a908041
flindeberg <flindeberg@gmail.com>
2019-09-03 23:36:06 +0200

    Updated scripts to better handle image generation

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -15,9 +15,12 @@ echo "Converted and got ncol-file $ncol"
 # run.sh uses $ncol variable
 ./run.sh -C
 
+echo "Sleeping for a couple of second to wait in image generation"
+sleep 5
+
 echo "Moving images, png to '${rundir}' and '../../resource/images/' for easy commit."
 # take care of the images
-cp tmp/*/*.png ${filename}.png
+cp tmp/*/0.coords*.png ${filename}.png
 cp ${filename}.png ../../resources/images/.
 
 echo "Moving graph-components (lgl and coords)"
--- scripts/create_run.sh
+++ scripts/create_run.sh
@@ -2,11 +2,18 @@
 
 ## creates a testrun named $1
 
+if [ -d "../testrun/${1}" ];
+then
+    ## bkp it
+    echo "Directory ${1} exists, moving it." 
+    mv "../testrun/${1}" "../testrun/${1}_old";
+fi
+
 mkdir -p ../testrun/$1
 
 ## copy and make executable
 cp run.sh parsebview.sh bootstrap.sh ../testrun/$1/
 chmod +x ../testrun/$1/*.sh
 
+# should be done
 echo "Now 'cd ../testrun/$1', copy in bgp-dump and run './bootstrap.sh routingfile.gz'"
-
